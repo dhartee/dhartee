3c3a3f73dd8e81d65db65ccbac2326b08f44fcd3
DharTee <121927489+dhartee@users.noreply.github.com>
2025-10-27 19:43:44 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,11 @@
- const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDtETZfOQROkx-Gi8gyFNBGjI9Iz8zPNPQ",
     authDomain: "dhartee-blog.firebaseapp.com",
     projectId: "dhartee-blog",
-    storageBucket: "dhartee-blog.firebasestorage.app",
+    storageBucket: "dhartee-blog.appspot.com",
     messagingSenderId: "773436527077",
-    appId: "1:773436527077:web:67e36607299420ace9df0c",
-    measurementId: "G-TEDKTPHB64"
+    appId: "1:773436527077:web:67e36607299420ace9df0c"
   };
+
+// Initialize Firebase App
+firebase.initializeApp(firebaseConfig);
